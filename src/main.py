28abccd749d3f800e1c631da285a77a04ed0af5c
SynneK1337 <synnek1337@protonmail.com>
2019-12-04 22:05:06 +0100

    Fixed bug that reports TCP Scan results as UDP.

--- src/main.py
+++ src/main.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     if not args.ipv6:
         result = scanner.scan_ipv4_tcp(args.hostname, args.port)
         if result:
-            print(f"[+] {args.hostname}:{args.port}/udp is open.")
+            print(f"[+] {args.hostname}:{args.port}/tcp is open.")
         elif result is False:
-            print(f"[-] {args.hostname}:{args.port}/udp is closed.")
+            print(f"[-] {args.hostname}:{args.port}/tcp is closed.")
         else:
             print(f"[!] {args.hostname} is inaccessible.")
 
